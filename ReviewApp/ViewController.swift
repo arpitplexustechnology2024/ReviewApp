2e506d844bacf24720401556f51a7b5b43b4338e
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-05-30 18:54:09 -0700

    Commit

--- ReviewApp/ViewController.swift
+++ ReviewApp/ViewController.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import StoreKit
+
+var BaseURL = ""
+var AppName = Bundle.main.infoDictionary?[kCFBundleNameKey as String] as? String
 
 class ViewController: UIViewController {
 
@@ -13,7 +17,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func btnShareAppTapped(_ sender: UIButton) {
+        print("Tapped Share", AppName ?? "")
+        guard let appName = AppName else { return }
+        self.share(items: [appName, URL.init(string: BaseURL) as Any])
+    }
+    
+    @IBAction func btnReviewAppTapped(_ sender: UIButton) {
+        rateUs()
+    }
+    
+    func rateUs() {
+        if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+            DispatchQueue.main.async {
+                SKStoreReviewController.requestReview(in: scene)
+            }
+        } else {
+            print(" - - - - - - Rating view in not present - - - -")
+        }
+    }
+    
+    func share(items : [Any]) {
+        let activityController = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        activityController.popoverPresentationController?.sourceView = self.view
+        self.present(activityController, animated: true, completion: nil)
+    }
 
 }
 
